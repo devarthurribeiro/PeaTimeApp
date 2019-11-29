656b9ebf4ab2667f8704ce437b7ddfe7dea1d57c
Macbook pro retina <devarthurribeiro@gmail.com>
2019-11-29 03:38:58 -0300

    tela inicial e definicao de tema

--- app/src/main/java/com/tads/peatime/ui/dashboard/TimesFragment.kt
+++ app/src/main/java/com/tads/peatime/ui/dashboard/TimesFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.tads.peatime.R
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class TimesFragment : Fragment() {
 
@@ -26,6 +30,18 @@ class TimesFragment : Fragment() {
         timesViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        var huors = mutableListOf("06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00")
+
+        var adapter = ArrayAdapter<String>(
+            root.context, // Context
+            android.R.layout.simple_spinner_item, // Layout
+            huors
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        root.spinner.adapter = adapter
+
         return root
     }
 }
--- app/src/main/java/com/tads/peatime/ui/dashboard/TimesViewModel.kt
+++ app/src/main/java/com/tads/peatime/ui/dashboard/TimesViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class TimesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Horários"
     }
     val text: LiveData<String> = _text
 }
