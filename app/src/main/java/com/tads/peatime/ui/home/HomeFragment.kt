8002be9a7d096d5f04312fd27e294569d8b8da1c
Macbook pro retina <devarthurribeiro@gmail.com>
2019-12-05 10:08:17 -0300

    botao alimentar na home

--- app/src/main/java/com/tads/peatime/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tads/peatime/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.tads.peatime.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.tads.peatime.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import java.net.URL
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +30,18 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        root.btnFeed.setOnClickListener {
+            sendGet("open", root.context)
+        }
+
         return root
+    }
+
+    fun sendGet(path:String, ctx: Context) {
+        val url = URL("http://10.77.15.64:3000/$path")
+        Toast.makeText(ctx, "Comida", Toast.LENGTH_LONG).show()
+        val result = url.readText()
+        println(result)
     }
 }
