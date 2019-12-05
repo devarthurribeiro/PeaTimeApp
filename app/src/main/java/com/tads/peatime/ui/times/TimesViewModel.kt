6b23adcbae444b72acfffd51a38f7ecf0b32c1a0
PedroRicardo117 <pedro.ricardo575@gmail.com>
2019-12-05 09:37:54 -0300

    Traduções refeitas e corrigidas.

--- app/src/main/java/com/tads/peatime/ui/times/TimesViewModel.kt
+++ app/src/main/java/com/tads/peatime/ui/times/TimesViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class TimesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Horários"
+        //value = "Horários"
     }
     val text: LiveData<String> = _text
 }
